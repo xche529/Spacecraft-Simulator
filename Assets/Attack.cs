d5b4583413c3bc91d0a563cdf6b4becbe55177d8
Ruotong-Betty-Zhang <126358256+Ruotong-Betty-Zhang@users.noreply.github.com>
2023-05-06 12:50:27 +1200

    add code for one gun and planet

---
+++ Assets/Attack.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Transform gunEnd; // 枪口位置
+    public int damage = 10; // 武器的攻击力
+    public float range = 100f; // 武器的攻击范围
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gunEnd.position, gunEnd.forward, out hit, range))
+        {
+            // 如果击中了植物，则获取植物的 Life 组件，并调用 TakeDamage 方法
+            Life life = hit.transform.GetComponent<Life>();
+            if (life != null)
+            {
+                life.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Life.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Life : MonoBehaviour
+{
+    public float health = 100f; // 生命值
+    public GameObject deathEffect; // 死亡特效
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage; // 减少生命值
+
+        if (health <= 0f)
+        {
+            Die(); // 如果生命值小于等于 0，则死亡
+        }
+    }
+
+    private void Die()
+    {
+        if (deathEffect != null)
+        {
+            Instantiate(deathEffect, transform.position, transform.rotation); // 播放死亡特效
+        }
+
+        Destroy(gameObject); // 销毁游戏对象
+    }
+}
